1e56626c94bd7dc74aaaf669e31f7c8b6bf6abf4
Antonio Cruz <acruz9823@gmail.com>
2020-05-21 03:09:25 +0100

    Client Send Operation Handler

--- client/public/client.js
+++ client/public/client.js
@@ -1,5 +1,8 @@
 const getToken = document.getElementById("getToken");
 const getResource = document.getElementById("getResource");
+const operationBtn = document.getElementById("postOperation");
+
+
 let client_id = "client";
 let client_secret = "123";
 let redirect_uris = "http://localhost:9000";
@@ -12,5 +15,35 @@ const postInfo = () => {
 const postToken = () => {
 
 };
+const postOperation = (event) => {
+   let acessToken = document.getElementById("token").value;
+   const resourceServer = axios.create({
+      baseURL: 'http://localhost:9002',
+      timeout: 5000
+   });
+
+   word = document.getElementById('resource').value;
+   resourceServer.interceptors.request.use((config) => {
+      if(acessToken)
+         config.headers['Authorization'] = `Bearer ${acessToken}`;
+      return config;;
+   })
+   if (document.getElementById('read').checked) {
+      acessToken.get('/' + word);
+      return;
+    }
+    
+    if (document.getElementById('delete').checked) {
+      acessToken.delete('/' + word);
+      return;
+    }
+
+    if (document.getElementById('delete').checked) {
+      acessToken.post('/' + word);
+      return;
+    }
+
+};
 getToken.addEventListener('click',postInfo);
 getResource.addEventListener('click',postToken);
+operationBtn.addEventListener('click',postOperation);
